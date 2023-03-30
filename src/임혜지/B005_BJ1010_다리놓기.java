5526416fb7c8be64b4badfc77865e73d21042607
hj <ihj028003@gmail.com>
2023-03-30 10:37:05 +0900

    주석 완

--- src/임혜지/B005_BJ1010_다리놓기.java
+++ src/임혜지/B005_BJ1010_다리놓기.java
@@ -1,5 +1,41 @@
 package 임혜지;
 
-public class B005_BJ1010_다리놓기 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class B005_BJ1010_다리놓기 {
+	static int t;// 테스트 케이스 수
+	static int n, m;// 서쪽 개수, 동쪽 개수
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));// 버퍼로 입력
+		StringTokenizer st = null;// 띄어쓰기로 구분해 입력받을 준비
+
+		t = Integer.parseInt(in.readLine());// 테스트 케이스 수 입력
+
+		for (int test_case = 1; test_case <= t; test_case++) {
+			st = new StringTokenizer(in.readLine());// 한 줄 입력
+
+			n = Integer.parseInt(st.nextToken());// 서쪽 개수 입력
+			m = Integer.parseInt(st.nextToken());// 동쪽 개수 입력
+
+			System.out.println(com(m, n));// 각각의 결과(다리 놓을 수 있는 경우의 수) 출력
+		}
+	}
+
+	static int com(int n, int k) {// 조합
+		int[][] temp = new int[n + 1][n + 1];// n번째에 대해 계산하므로, n+1의 사이즈로 배열 생성
+
+		for (int i = 0; i <= n; i++) {// 모든 숫자에 대해
+			for (int j = 0; j <= Math.min(i, k); j++) {
+				if (j == 0 || j == i)// 양끝 값의 경우
+					temp[i][j] = 1;// 조합에서의 값이 1
+				else
+					temp[i][j] = temp[i - 1][j - 1] + temp[i - 1][j];// 조합 점화식
+			}
+		}
+		return temp[n][k];// 조합 점화식을 통해 누적된 결과 리턴
+	}
 }
--- src/임혜지/B006_BJ17070_파이프옮기기1.java
+++ src/임혜지/B006_BJ17070_파이프옮기기1.java
@@ -1,5 +1,85 @@
 package 임혜지;
 
-public class B006_BJ17070_파이프옮기기1 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
+public class B006_BJ17070_파이프옮기기1 {
+	static int n;// 배열 사이즈
+	static int arr[][];// 입력받을 배열
+	static int width[][];// 가로 방향 경우의 수
+	static int height[][];// 세로 방향 경우의 수
+	static int diagonal[][];// 대각선 방향 경우의 수
+	static int res = 0;// 결과로 출력할 총 경우의 수
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));// 버퍼로 입력
+		StringTokenizer st = null;// 띄어쓰기로 구분해 입력받을 준비
+
+		n = Integer.parseInt(in.readLine());// 배열 사이즈 입력
+
+		arr = new int[n][n];// 입력받을 배열 생성
+		width = new int[n][n];// 가로방향 파이프의 경우의 수를 담을 배열 생성
+		height = new int[n][n];// 세로방향 파이프의 경우의 수를 담을 배열 생성
+		diagonal = new int[n][n];// 대각선방향 파이프의 경우의 수를 담을 배열 생성
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(in.readLine());// 한 줄 입력
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());// 빈칸(0)과 벽(1) 입력
+			}
+		}
+
+		sol(0, 1, 0);// (0,1)에서 가로방향(0)으로 시작
+		System.out.println(res);// 총 경우의 수 출력
+	}
+
+	static void sol(int row, int col, int dir) {// 현재의 위치와 파이프 방향
+		if (row == n - 1 && col == n - 1) {// 끝까지 도달했다면
+			res++;// 경우의 수 1 증가시키고
+			return;// 종료
+		}
+
+		if (dir == 0) {// 가로
+			if (col - 1 >= 0 && arr[row][col] != 1) {// 배열의 인덱스를 벗어나지 않으면서 벽이 아닌 경우
+				width[row][col] = width[row][col - 1] + diagonal[row][col - 1];// 가로 방향은 왼쪽 가로와 왼쪽 대각선 경우의 수 더함
+				if (col + 1 < n && arr[row][col + 1] != 1) {// 가로 방향으로 나아갈 수 있다면
+					sol(row, col + 1, 0);// 재귀 호출
+				}
+				if (row + 1 < n && col + 1 < n && arr[row + 1][col + 1] != 1 && arr[row][col + 1] != 1
+						&& arr[row + 1][col] != 1) {// 대각선 방향으로 나아갈 수 있다면
+					sol(row + 1, col + 1, 2);// 재귀 호출
+				}
+			}
+		}
+		if (dir == 1) {// 세로
+			if (row - 1 >= 0 && arr[row][col] != 1) {// 배열의 인덱스를 벗어나지 않으면서 벽이 아닌 경우
+				height[row][col] = height[row - 1][col] + diagonal[row - 1][col];// 세로 방향은 위쪽 세로와 위쪽 대각선 경우의 수 더함
+				if (row + 1 < n && arr[row + 1][col] != 1) {// 세로 방향으로 나아갈 수 있다면
+					sol(row + 1, col, 1);// 재귀 호출
+				}
+				if (row + 1 < n && col + 1 < n && arr[row + 1][col + 1] != 1 && arr[row][col + 1] != 1
+						&& arr[row + 1][col] != 1) {// 대각선 방향으로 나아갈 수 있다면
+					sol(row + 1, col + 1, 2);// 재귀 호출
+				}
+			}
+		}
+		if (dir == 2) {// 대각선
+			if (col - 1 >= 0 && row - 1 >= 0 && arr[row][col] != 1) {// 배열의 인덱스를 벗어나지 않으면서 벽이 아닌 경우
+				diagonal[row][col] = width[row - 1][col - 1] + height[row - 1][col - 1] + diagonal[row - 1][col - 1];
+				// 대각선 방향은 왼위쪽 가로와 왼위쪽 세로와 왼위쪽 대각선 경우의 수 더함
+				if (col + 1 < n && arr[row][col + 1] != 1) {// 가로 방향으로 나아갈 수 있다면
+					sol(row, col + 1, 0);// 재귀 호출
+				}
+				if (row + 1 < n && arr[row + 1][col] != 1) {// 새로 방향으로 나아갈 수 있다면
+					sol(row + 1, col, 1);// 재귀 호출
+				}
+				if (row + 1 < n && col + 1 < n && arr[row + 1][col + 1] != 1 && arr[row][col + 1] != 1
+						&& arr[row + 1][col] != 1) {// 대각선 방향으로 나아갈 수 있다면
+					sol(row + 1, col + 1, 2);// 재귀 호출
+				}
+			}
+		}
+	}
 }
